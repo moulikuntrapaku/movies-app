faaa64a6ab87cf3f1a236859024b1860e34478eb
moulikuntrapaku <moulikuntrapaku@gmail.com>
2025-10-06 20:33:28 +0530

    replace hardcoded movies with API data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,19 @@ import './home.css'
 export default function Home(){
     const [movies, updateMovies] = useState([])
 
+        
+    const getMovieRequest = async () => {
+        const url = "http://www.omdbapi.com/?s=star&apikey=429d5e5f"
+        const response = await fetch(url)
+
+        const responseJson = await response.json()
+        updateMovies(responseJson.Search)
+    }
+
+    useEffect(()=>{
+        getMovieRequest()
+    },[])
+
     return(
         <div className="movie-container">
             {movies.map((movie) => (
